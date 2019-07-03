b1b80785b7e85269d79cd07050262d24fbbaede3
reshmaj <reshmaj@qburst.com>
2019-07-03 19:20:23 +0530

    finding correct source file to comment

--- modules/stock.src.js
+++ modules/stock.src.js
@@ -5148,7 +5148,8 @@
                         isYTDButNotSelected = !isSelected;
                     } else if (type === 'all') {
                         isSameRange = baseAxis.max - baseAxis.min >= dataMax - dataMin;
-                        isAllButAlreadyShowingAll = !isSelected && selectedExists && isSameRange;
+                         /**2. Commented code to show 'All' option always*/
+                        // isAllButAlreadyShowingAll = !isSelected && selectedExists && isSameRange;
                     }
                     // The new zoom area happens to match the range for a button - mark it selected.
                     // This happens when scrolling across an ordinal gap. It can be seen in the intraday
